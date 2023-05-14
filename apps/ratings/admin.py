6a3776459a429c55afb7c396a0637f96f95b3732
unknown <Erick@ERICK-NDUNGU.AD1.salixkenya.com>
2023-05-14 18:09:17 +0300

    rating model and serializer

--- apps/ratings/admin.py
+++ apps/ratings/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+
+from .models import Rating
+
+
+class RatingAdmin(admin.ModelAdmin):
+    list_display = ["rater", "agent", "rating"]
+
+
+admin.site.register(Rating, RatingAdmin)
