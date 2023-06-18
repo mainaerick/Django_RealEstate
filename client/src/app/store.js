e56b4e58620059d285fa9e30ab8cbcc353a0d00a
mainaerick <niteric@gmail.com>
2023-06-18 23:04:12 +0300

    add property slice

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,6 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
+import propertySlice from '../features/properties/propertySlice';
 
 export const store = configureStore({
   reducer: {
+    properties:propertySlice
   },
 });
--- client/src/pages/PropertiesPage.jsx
+++ client/src/pages/PropertiesPage.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Col, Container, Row } from "react-bootstrap";
+import Spinner from "../components/Spinner";
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-toastify";
+import { getProperties } from "../features/properties/propertySlice";
 
 const PropertiesPage = () => {
+  const { properties, isLoading, isError, message } = useSelector(
+    (state) => state.properties
+  );
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message, { icon: "😭" });
+    }
+    dispatch(getProperties());
+  }, [dispatch, isError, message]);
+
+
+  if (isLoading) {
+		return <Spinner />;
+	}
   return (
     <div>
       <Container>
